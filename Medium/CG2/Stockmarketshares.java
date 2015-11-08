dc3015e22e109889ac2d47be10a3f25186d9dcdd
piwatake <piwatake2011@knights.ucf.edu>
2015-11-08 09:34:05 -0500

    up to 92% on stock puzzle.

--- Medium/CG2/Stockmarketshares.java
+++ Medium/CG2/Stockmarketshares.java
@@ -38,43 +38,62 @@ class Solution {
 	    int min = Integer.parseInt(myarr[0]);
 	    int maxpos = 0;
 	    int minpos = 0;
+	    int offset = 0;
 	    //main work loop
-	    for(int i = 0 ; i<n;i++){
-	        int curr = Integer.parseInt(myarr[i]);
 	        
-	        if(curr>max){
-	            max = curr;
-	            maxpos = i;
-	        }
-	        
-	        if(curr<min){
-	            min = curr;
-	            minpos = i;
-	        }
-	        
-	        if(i>0){
-	            if(Integer.parseInt(myarr[i]) > Integer.parseInt(myarr[i-1])){
-	                incr++;
-	            } else if (Integer.parseInt(myarr[i]) < Integer.parseInt(myarr[i-1])){
-	                decr++;
-	            } 
-	        }
-	        System.err.println(myarr[i]);
-	    }
+	        for(int i = 0 ; i<n-offset;i++){
+    	        int curr = Integer.parseInt(myarr[i]);
+    	        
+    	        if(curr>max){
+    	            max = curr;
+    	            maxpos = i;
+    	        }
+    	        
+    	        if(curr<min){
+    	            min = curr;
+    	            minpos = i;
+    	        }
+    	        
+    	        if(i>0){
+    	            if(Integer.parseInt(myarr[i]) > Integer.parseInt(myarr[i-1])){
+    	                incr++;
+    	            } else if (Integer.parseInt(myarr[i]) < Integer.parseInt(myarr[i-1])){
+    	                decr++;
+    	            } 
+    	        }
+    	        System.err.println(myarr[i]);
+    	    }
+    	    
+    	    
+    	    int maxfrontofmin = 0;
+    	    for(int j = minpos ; j>=0 ;j-- ){
+    	        int curr = Integer.parseInt(myarr[j]);
+    	        if(curr > maxfrontofmin){
+    	            maxfrontofmin = curr;
+    	        }
+    	        
+    	        
+    	    }
+    	    
+    	    System.err.println("maxfrontof min is "+maxfrontofmin+" and diff is "+(min-maxfrontofmin));
+    	    
+
+    	    
 	    
+	    System.err.println("Maxpos is "+maxpos+" and min pos is "+minpos);
 	    
 	    
 	    
 	    
 	    //begin printing out answers.
 	    
-	    System.err.println(incr+" "+decr);
+	    //System.err.println(incr+" "+decr);
 	    
-	    if(incr > 0 && decr == 0){
+	    if((incr > 0 && decr == 0 ) || incr == 0 && decr == 0){
 	        System.out.println("0");
 	    } else if (decr > 0 && incr == 0){
 	        System.out.println(Integer.parseInt(myarr[1]) - Integer.parseInt(myarr[0]));
-	    } else {
+	    } else {/*
 	        int offset = 0;
             while(maxpos > minpos){
                 System.err.println("Maxpos is "+maxpos+" and min pos is "+minpos);
@@ -112,11 +131,11 @@ class Solution {
         	            } 
         	        }
         	        System.err.println(myarr[i]);
-        	    }
+        	    }*/
     	        
     	        System.err.println("problem.");
-    	        System.out.println(Integer.parseInt(myarr[minpos]) - Integer.parseInt(myarr[maxpos]));
-    	    }
+    	        System.out.println((min-maxfrontofmin));
+    	    //}
 	    }
 	    
 
